c04a2a95aff57cd001723c5e3cb199b4cadc703b
Роман <https://github.com/Roman793611/homework>
2025-12-21 19:23:47 +0200

    java 2

---
+++ js 2/task-1.js
@@ -0,0 +1,48 @@
+// 1. Переведення температури з Цельсія у Фаренгейти
+let celsius = 25;
+let fahrenheit = celsius * 9 / 5 + 32;
+console.log("Температура у Фаренгейтах:", fahrenheit);
+
+// 2. Кількість годин і хвилин у місяці
+let daysInMonth = 30;
+let hoursInMonth = daysInMonth * 24;
+let minutesInMonth = hoursInMonth * 60;
+console.log("Годин у місяці:", hoursInMonth);
+console.log("Хвилин у місяці:", minutesInMonth);
+
+let health = 100;
+let energy = 80;
+health = health - 20;
+energy = energy - 15;
+console.log("Рівень здоров'я:", health);
+console.log("Рівень енергії:", energy);
+
+let totalPrice = 1000;
+let discount = 0.10;
+let discountedPrice = totalPrice * (1 - discount);
+console.log("Ціна зі знижкою:", discountedPrice);
+
+const floatNumber = 12.78;
+let roundedDown = Math.floor(floatNumber);
+console.log("Округлене число:", roundedDown);
+
+const floatString = "45.67";
+let parsedFloat = parseFloat(floatString);
+console.log("Десяткове число:", parsedFloat);
+
+const intString = "123";
+let parsedInt = parseInt(intString);
+console.log("Ціле число:", parsedInt);
+
+let number = 81;
+let sqrtNumber = Math.sqrt(number);
+console.log("Квадратний корінь:", sqrtNumber);
+
+const integer = 42;
+const stringNumber = "256";
+
+let convertedInt = parseInt(stringNumber);
+console.log("Перетворений у число:", convertedInt);
+
+let convertedString = integer.toString();
+console.log("Перетворений у рядок:", convertedString);
